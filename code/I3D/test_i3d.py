29fa514f716f21c136f86c9686de2f1dc618d048
Joshua Lo <33582835+joshrclo@users.noreply.github.com>
2021-05-09 17:55:31 -0400

    Update test_i3d.py

--- code/I3D/test_i3d.py
+++ code/I3D/test_i3d.py
@@ -61,7 +61,7 @@ def run(init_lr=0.1,
     # setup dataset
     test_transforms = transforms.Compose([videotransforms.CenterCrop(224)])
     
-    val_dataset = Dataset(train_split, 'test', root, mode, test_transforms)
+    val_dataset = Dataset(train_split, 'test', root, mode, num_classes, test_transforms)
     val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=1,
                                                  shuffle=False, num_workers=2,
                                                  pin_memory=False)
