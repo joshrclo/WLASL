772a8f644293b67923d0caba45ec947aaaca8e9c
Joshua Lo <33582835+joshrclo@users.noreply.github.com>
2021-05-10 00:28:13 -0400

    Update test_i3d.py

--- code/I3D/test_i3d.py
+++ code/I3D/test_i3d.py
@@ -121,6 +121,9 @@ def run(init_lr=0.1,
             top1_tp[labels[0].item()] += 1
         else:
             top1_fp[labels[0].item()] += 1
+            
+        if len(dataloaders['test']) < 10:
+            print("label {}, top-10 predictions {}".format(labels[0].item(), out_labels[-10:]))
 #         print(video_id, float(correct) / len(dataloaders["test"]), float(correct_5) / len(dataloaders["test"]),
 #               float(correct_10) / len(dataloaders["test"]))
     print('top 1', float(correct) / len(dataloaders["test"]), 'top 5', float(correct_5) / len(dataloaders["test"]),
